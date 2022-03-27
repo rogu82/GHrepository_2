ae10af5d15ef1bfbb474468379d4bd8c88c67582
rogu82 <black175around@outlook.jp>
2022-03-27 23:54:33 +0900

    カリキュラム8-4課題4：ブログ投稿作成画面の保存時入力チェック処理実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; 
+
 
 class PostController extends Controller
 {
@@ -22,7 +23,7 @@ public function create()
     return view('posts/create');
 }
 
-public function store(Request $request, Post $post)
+public function store(Post $post, PostRequest $request)
 {
     $input = $request['post'];
     $post->fill($input)->save();
