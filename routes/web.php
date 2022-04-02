6dbc3a815dd917bd72333314290f4e0ce5ee6cae
rogu82 <black175around@outlook.jp>
2022-04-02 17:45:06 +0900

    カリキュラム8-5　ブログ投稿の編集

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,12 @@
 
 Route::get('/','PostController@index');
 Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+
+Route::put('/posts/{post}', 'PostController@update');
+
 Route::get('/posts/{post}', 'PostController@show');
 
 Route::post('/posts', 'PostController@store');
 
+Route::put('/posts/{post}', 'PostController@update');
